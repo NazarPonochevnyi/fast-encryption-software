09af826c1b6ee8fd49a7fa3f02be7282bd7e6d6b
Nazar Ponochevnyi <Nazar Ponochevnyi>
2017-05-02 13:49:03 +0300

    Fixed bugs.

--- сipher_of_сaesar.py
+++ сipher_of_сaesar.py
@@ -1,33 +1,43 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
 '''
 @Caesar's Cipher@
 
 Comments:
---------
+You can change the array with the alphabet to your language,
+and the program will work with your language.
+
+Also, pay attention to the line with the switch, changing +/-,
+you can change the program for encryption / decryption.
+
+Author: Ponochevnyi Nazar
 '''
 
 
-#Alphabet
+#Alphabet (You can change)
 A = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
 
 
 #Input
-your_text = input('Input text: ')
-key = int(input('Input key: '))
+your_text = input('\nInput text: ')
+key = int(input('\nInput key: '))
 your_text = your_text.upper()
 
 
 #Basic proccess
 result = ''
 for t in your_text:
-    if t != ' ':
+    if A.count(t) != 0:
         pos = A.index(t)
-        pos += key #    !!! Toggle (+/-) !!!
-        if pos > len(A) - 1:
+        pos += key     #!!! Toggle (+/-) !!!
+        if pos >= len(A):
             pos -= len(A)
         result += A[pos]
     else:
-        result += ' '
+        result += t
 
 
 #Output
-print('Enctypted text:', result)
+print('\nEncrypted text:', result)
