73f045aec39b1e4485742cc009e2772aa95f0290
Nazar Ponochevnyi <Nazar Ponochevnyi>
2017-05-02 13:38:11 +0300

    Added help window and comments.

--- сipher_of_сaesar.py
+++ сipher_of_сaesar.py
@@ -1,25 +1,33 @@
-import random
+'''
+@Caesar's Cipher@
+
+Comments:
+--------
+'''
 
 
-#
-
+#Alphabet
 A = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
 
 
+#Input
 your_text = input('Input text: ')
+key = int(input('Input key: '))
 your_text = your_text.upper()
-key = random.randint(1,25)
 
 
+#Basic proccess
 result = ''
 for t in your_text:
     if t != ' ':
         pos = A.index(t)
-        pos += key
+        pos += key #    !!! Toggle (+/-) !!!
         if pos > len(A) - 1:
             pos -= len(A)
         result += A[pos]
     else:
         result += ' '
 
-print(result)
+
+#Output
+print('Enctypted text:', result)
