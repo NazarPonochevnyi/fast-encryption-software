4f571db6bfa41a8b98dae02eca29081c83dade9d
NazarPonochevnyi <nazar.ponochevnyi@gmail.com>
2017-09-10 18:10:58 +0300

    Everything is set up and fixed bugs

--- vernam_cipher.py
+++ vernam_cipher.py
@@ -12,11 +12,12 @@ Author: Nazar Ponochevnyi
 '''
 
 # Import some module
+import ast
 import numpy as np # pip install numpy
 
 
 # XOR function
-def slow_xor(aa, bb):
+def xor(aa, bb):
     a = np.frombuffer(aa, dtype=np.byte)
     b = np.frombuffer(bb, dtype=np.byte)
     return np.bitwise_xor(a, b).tostring()
@@ -24,15 +25,27 @@ def slow_xor(aa, bb):
 
 # Input values
 while True:
-    text = input('\nInput text: ').encode()
-    key = input('\nInput key: ').encode()
-    if len(text) == len(key): break
+    action = input('\nInput action: ').strip()
+    if action in ['en', 'de']: break
+    else: print('\nIncorrect action!')
+while True:
+    text = ast.literal_eval("b'{}'".format(input('\nInput text: ').strip().replace('\'', '`')))
+    key = ast.literal_eval("b'{}'".format(input('\nInput key: ').strip().replace('\'', '`')))
+    if len(key) >= len(text):
+        key = key[:len(text)]
+        break
     print('\nInput text and a key of equal length!\n')
 
 
 # Basic processes
-result = slow_xor(text, key)
+result = xor(text, key)
 
 
 # Output values
-print('\nEncrypted/Decrypted text:', result)
+if action == 'en':
+    result = str(result)
+    print('\nEncrypted text:')
+    print(result.replace('b\'', '').replace(result[-2] + '\'', result[-2]))
+else:
+    print('\nDecrypted text:')
+    print(result.decode().replace('`', '\''))
