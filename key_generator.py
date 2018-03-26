2220436061e787b087d72079f3802d49643b693c
Nazar Ponochevnyi <nazar.ponochevnyi@gmail.com>
2018-03-26 18:14:51 +0300

    Update key_generator.py

--- key_generator.py
+++ key_generator.py
@@ -10,3 +10,16 @@ Coming soon you can generating keys for you message!
 
 Author: Ponochevnyi Nazar
 '''
+
+import random
+
+output_file = 'key.txt' # name of the file in which the key is saved
+length_key = 1000000 # required key length
+
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] # you can change
+
+result = ''.join([random.choice(alphabet) for _ in range(length_key)])
+
+file = open(output_file, 'w')
+file.write(result)
+file.close()
